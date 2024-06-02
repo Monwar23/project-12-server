4bffaf60103499756124256ed0ec38c836e6c92a
monwar23 <monwar2345@gmail.com>
2024-06-02 21:22:53 +0600

    get data by id from pet collection

--- index.js
+++ index.js
@@ -140,6 +140,13 @@ async function run() {
         res.send(result)
       })
 
+      app.get('/allPets/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await petCollection.findOne(query);
+        res.send(result);
+      })
+
       // user
     
     app.get('/user/:email', async (req, res) => {
