f6abe91412de4b0856338f618459bd6eb6982039
monwar23 <monwar2345@gmail.com>
2024-06-02 20:25:16 +0600

    update user role

--- index.js
+++ index.js
@@ -147,7 +147,17 @@ async function run() {
       res.send(result)
     })
 
-
+    app.patch('/users/:id',verifyToken,verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+          $set: {
+              role: 'admin'
+          }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+  })
   
       app.put('/user', async (req, res) => {
         const user = req.body
